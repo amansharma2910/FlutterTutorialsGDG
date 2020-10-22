842d4f51366cd66574c04ef846cd34b6d1a6a32f
amansharma2910 <amansharma2910@gmail.com>
2020-10-22 17:06:10 +0530

    Trending section checkpoint

--- tmdb_movies/lib/widgets/trending_section.dart
+++ tmdb_movies/lib/widgets/trending_section.dart
@@ -6,13 +6,40 @@ class TrendingSection extends StatelessWidget {
     return Container(
       height: 300.0,
       width: MediaQuery.of(context).size.width,
-      color: Colors.blue,
-      padding: EdgeInsets.symmetric(vertical: 5.0),
-      child: ListView.builder(
-          itemBuilder: (BuildContext context, count) => TrendingCard(),
-          itemCount: 20,
-          scrollDirection: Axis.horizontal,
+      color: Colors.grey[100],
+      padding: EdgeInsets.all(25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              'Trending Movies',
+              style: TextStyle(
+                fontSize: 23.0,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          TrendingList(),
+        ],
       ),
+    );
+  }
+}
+
+
+class TrendingList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, count) => TrendingCard(),
+      itemCount: 20,
+      scrollDirection: Axis.horizontal,
     );
   }
 }
@@ -20,16 +47,7 @@ class TrendingSection extends StatelessWidget {
 class TrendingCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20.0),
-      height: 10,
-      width: 10,
-      color: Colors.green[100],
-      alignment: Alignment.center,
-      child: Column(
-
-      ),
-    );
+    return Container();
   }
 }
 

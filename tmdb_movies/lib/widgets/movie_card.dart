bf7002b2c9df6fdb9955e6625ce4ed04197d63be
amansharma2910 <amansharma2910@gmail.com>
2020-11-14 17:03:21 +0530

    Finished movie card, started work on trending card widget

--- tmdb_movies/lib/widgets/movie_card.dart
+++ tmdb_movies/lib/widgets/movie_card.dart
@@ -1,1 +1,35 @@
 import 'package:flutter/material.dart';
+
+class MovieCard extends StatelessWidget {
+  final double aspectRatio;
+  final double height;
+  final Widget stackChild;
+  final String imageUrl;
+
+  MovieCard({this.aspectRatio, this.height, this.stackChild, this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height ?? 150,
+      padding: EdgeInsets.only(right: 24.0),
+      child: AspectRatio(
+        aspectRatio: 300 / 150,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(imageUrl),
+                  ),
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+            ),
+            SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
